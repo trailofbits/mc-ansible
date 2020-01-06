680d7b87aa5ccd4a48a49fc4fa88f7fac8d0ed12
Philip Wang <powdercheeze@gmail.com>
2020-01-05 23:38:54 -0500

    Restructure repository, add manticore wrapper

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='mcorepv',
+    description="Provision manticore jobs locally or remotely",
+    version='0.1.0',
+    packages=['mcorepv'],
+    author="pwang00",
+    python_requires=">=3.6",
+    install_requires=[
+        "manticore",
+        "ansible>=2.8",
+        "gitpython"
+    ],
+    entry_points={'console_scripts': ['mcorepv = mcorepv.__main__:main']},
+)
