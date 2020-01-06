a3a1874d3f4d9a50c672c6f3ae92704c80ffd55a
Philip Wang <powdercheeze@gmail.com>
2020-01-05 23:41:01 -0500

    Add manticore wrapper

---
+++ mcorepv/__main__.py
@@ -0,0 +1,108 @@
+#!/usr/bin/python3
+""" Python wrapper built around supporting Manticore functionality"""
+
+import argparse
+import sys
+import subprocess
+import os
+import time
+import logging
+
+from .mcorepv_config import initial_setup
+
+logging.basicConfig(filename="mcprov.log", 
+    format='%(asctime)s %(message)s', 
+                filemode='w') 
+
+logger = logging.getLogger("mcprov.main")
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Wrapper around Manticore CLI")
+    destination = parser.add_argument_group("destination flags (local, remote)")
+
+    destination.add_argument(
+        "--local",
+        action="store_true"
+    )
+
+    destination.add_argument(
+        "--remote",
+        action="store_true"
+    )
+
+    parser.add_argument(
+        "--playbook",
+        type=str,
+        default="digitalocean.yml",
+        help="specify Ansible playbook"
+    )
+
+    parsed, other = parser.parse_known_args(sys.argv[1:])   
+
+    if not len(sys.argv[1:]) > 0:
+        print("Error: argv must be nonempty")
+        sys.exit(1)
+
+    if parsed.local ^ parsed.remote != 1:
+        print("Error: must specify exactly one of --remote or --local flags")
+        sys.exit(1)
+
+    return parsed, other
+
+
+def main(args=None):
+
+    if not os.path.exists(".mcore_config"):
+        logger.warning("Warning: .mcore_config directory not found.")
+        logger.warning("Starting initial config...")
+        initial_setup()
+
+    parsed, other = parse_arguments()
+
+    flags = [flag for flag in other if flag.startswith("--")]
+    files = [file for file in other if not file.startswith("--")]
+
+    process = None
+    file_exists_as_argument = True
+
+    if len(files) == 0:
+        file_exists_as_argument = False
+    
+    if parsed.local:
+
+        if not file_exists_as_argument:
+            print("Error: must specify script to run")
+            sys.exit(1)
+
+        elif not files[0].endswith(".py"):
+            to_run = ["manticore"]
+            to_run.extend([files[0]])
+            to_run.extend(flags)
+
+        else:
+            to_run = ["python3"]
+            to_run.extend([files[0]]) 
+
+        process = subprocess.Popen(to_run)
+
+    elif parsed.remote:
+        to_run = ["ansible-playbook"]
+        to_run.append(parsed.playbook)
+
+        if file_exists_as_argument:
+            to_run.append("--extra-vars")
+            to_run.append("manticore_script={}".format(files[0]))
+            
+        process = subprocess.Popen(to_run)
+
+    # Allows the user to prematurely kill the subprocess with KeyboardInterrupt
+
+    try:
+        while process is not None and process.poll() is None:
+            time.sleep(0.1)
+    except KeyboardInterrupt:
+        process.terminate()
+
+
+if __name__ == "__main__":
+    main()
---
+++ mcorepv/mcorepv_config.py
@@ -0,0 +1,50 @@
+import os
+import yaml
+import git
+import logging
+
+repo_url = "https://github.com/pwang00/Ansible-Manticore.git"
+ansible_yml_path = ".mcore_config/vars/ansible.yml"
+droplet_yml_path = ".mcore_config/vars/droplet.yml"
+
+ansible_vars = {
+	"manticore_env": None,
+	"manticore_script": None,
+	"remote_uname": None,
+	"working_dir": "{{ lookup('env','PWD') }}",
+	"logfile": None,
+}
+
+digitalocean_vars = {
+	"digital_ocean_api_token": "{{ lookup('env', 'DO_TOKEN') }}",
+	"ssh_key_name": None,
+	"droplet_region": None,
+	"droplet_size": None
+}
+
+def initial_setup():
+
+	print(f".mcore_config directory not found, cloning from {repo_url}")
+
+	git.Repo.clone_from(repo_url, '.mcore_config', 
+	        branch='master')
+
+	if input("Use default configuration? (y/n) ").lower() == "n":
+		
+		print("Configure ansible variables: \n")
+		
+		for var in ansible_vars.keys():
+			ansible_vars[var] = input(f"{var}: ")
+
+		print("Configure remote droplet variables: \n")
+		
+		for var in digitalocean_vars.keys():
+			digitalocean_vars[var] = input(f"{var}: ")
+
+		with open(ansible_yml_path) as ansible_config, open(droplet_yml_path) as droplet_config:
+			yaml.dump(ansible_vars, ansible_config, default_flow_style=False)
+			yaml.dump(digitalocean_vars, droplet_config, default_flow_style=False)
+		
+if __name__ == "__main__":
+	initial_setup()
+
